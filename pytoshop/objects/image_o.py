323e6d9f57c341c1ca1519468858563b1d3af7ee
Marc Villain <marc.villain@epita.fr>
2018-04-17 11:48:43 -0700

    [OK] Moving workspace and brush.

--- pytoshop/objects/image_o.py
+++ pytoshop/objects/image_o.py
@@ -1,7 +1,19 @@
+import numpy as np
+import cv2
+
 class Image:
 
 	def __init__(self, width, height, image_name=None):
-		pass
+		channels_count = 3
+
+		if image_name == None:
+			self.value = cv2.cvtColor(np.full((height, width, channels_count), 255, np.uint8), cv2.COLOR_BGR2RGB)
+		else:
+			self.value = imread(image_name)
+
+		self.width = width
+		self.height = height
+		self.bytesPerLine = width * channels_count
 
 	def draw(self, x, y, brush):
-		pass
+		cv2.circle(self.value, (x,y), brush.radius, brush.color, brush.thickness)
--- pytoshop/views/main_v.py
+++ pytoshop/views/main_v.py
@@ -1,57 +1,68 @@
 import sys
 sys.path.append('..')
 
+from pytoshop.controllers.main_c import MainController,DrawingBoardController
+from pytoshop.objects.brush_o import Brush
+
 from PyQt5.QtWidgets import QApplication,QWidget,QVBoxLayout,QLabel
 from PyQt5.QtGui import QImage,QPixmap,QPainter
-from pytoshop.controllers.main_c import MainController
+from PyQt5.QtCore import Qt
 
 class DrawingBoard(QLabel):
 
     def __init__(self, parent, width, height, image_name=None):
         super().__init__(parent)
-        self.controller = parent.controller
+        self.controller = DrawingBoardController(parent.controller, self)
         self.controller.createImage(500, 500)
+        self.brush = Brush()
         
-    def setImage(image, width, height, bytesPerLine):
-        image = QImage(image, width, height, bytesPerLine, QImage.Format_RGB888)
+    def displayImage(self, image):
+        image = QImage(image.value, image.width, image.height, image.bytesPerLine, QImage.Format_RGB888)
         pixmap = QPixmap(image)
         self.setPixmap(pixmap)
 
     def mousePressEvent(self, event):
-        self.controller.onMousePressed(event, True)
+        self.controller.onMousePressed(event)
 
     def mouseMoveEvent(self, event):
-        self.controller.onMouseMove(event, True)
+        self.controller.onMouseMove(event)
 
 class MainView(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        drawing_board = DrawingBoard(self, 500, 500)
-
-        self.controller = MainController(self, drawing_board)
+        self.controller = MainController(self)
+        self.drawing_board = DrawingBoard(self, 500, 500)
 
         self.setGeometry(0, 0, 600, 600)
 
     # EVENTS #
 
     def keyPressEvent(self, event):
-        self.controller.onKeyPressed(event)
+        if event.key() == Qt.Key_Control:
+            self.controller.onControlPressed()
 
     def keyReleaseEvent(self, event):
-        self.controller.onKeyReleased(event)
+        if event.key() == Qt.Key_Control:
+            self.controller.onControlReleased()
 
     def mousePressEvent(self, event):
-        self.controller.onMousePressed(event, False)
+        self.controller.onMousePressed(event.globalPos())
 
     def mouseReleaseEvent(self, event):
-        self.controller.onMouseReleased(event)
+        self.controller.onMouseReleased()
 
     def mouseMoveEvent(self, event):
-        self.controller.onMouseMove(event, False)
+        self.controller.onMouseMove(event.globalPos())
 
     # FUNCTIONS #
 
-    def changeCursor(self, cursor):
-        QApplication.setOverrideCursor(cursor)
+    def showOpenHandCursor(self):
+        QApplication.setOverrideCursor(Qt.OpenHandCursor)
+
+    def showClosedHandCursor(self):
+        QApplication.setOverrideCursor(Qt.ClosedHandCursor)
+
+    def showArrowCursor(self):
+        QApplication.setOverrideCursor(Qt.ArrowCursor)
