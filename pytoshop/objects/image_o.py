5ae60c3c008fb1fd1ff7e4313370c38a50c19783
Marc Villain <marc.villain@epita.fr>
2018-05-02 16:05:24 -0700

    [ADD] Integration of save and load functions. Still need to re-do the file selection with the gui.

--- pytoshop/objects/image_o.py
+++ pytoshop/objects/image_o.py
@@ -21,6 +21,15 @@ class Image:
 
         self.top_layer = self.newLayer()
 
+    def load(self, image_name):
+        image = cv2.imread(image_name, -1)
+        # TODO: 1) Create new layer with image (careful with the size that can be bigger or smaller than the layer)
+        #       2) OR apply the image to the current layer (careful again about size)
+        pass
+
+    def save(self, location):
+        cv2.imwrite(location, self.top_layer.bottom_layer.rgba_display)
+
     def newLayer(self):
         bottom_layer = self.layers[-1]
         new_layer = Layer(self, bottom_layer)
