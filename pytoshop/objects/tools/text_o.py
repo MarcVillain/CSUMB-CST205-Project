761d31dcfa051e519902eac7ba456484a369f7b0
Marc Villain <marc.villain@epita.fr>
2018-05-12 14:32:10 -0700

    [FIX] Fixing merges.

--- pytoshop/objects/tools/text_o.py
+++ pytoshop/objects/tools/text_o.py
@@ -3,14 +3,13 @@ import numpy as np
 from pytoshop.objects.tool_o import Tool
 from pytoshop.views.main.top_bars.top_bar_text import TopBarText
 
+
 class Text(Tool):
-    def __init__(self, main_c, view, width, height, image_name=None):
+
+    def __init__(self):
         super().__init__()
-        self.main_c = main_c
-        self.view = view
-        self.width = width
-        self.height = height
-        self.text = "Hello World"
+
+        self.text = ""
         self.fontScale = 2
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.color = (0, 0, 0, 255)
@@ -20,16 +19,6 @@ class Text(Tool):
 
         controller.image.current_layer.draw(self.generate(controller, x0, y0), controller.image.width//2, controller.image.height//2)
         controller.view.refresh()
-
-    def onMouseMove(self, controller, x0, y0, x1, y1):
-        # main_c.image.top_layer.clear()
-        # main_c.image.top_layer.draw(self.generate(), x1, y1)
-
-        #self.main_c.view.refresh()
-        pass
-
-    def onMouseReleased(self, controller):
-        pass
 
     def generate(self, controller, x0, y0):
         height = controller.image.height
