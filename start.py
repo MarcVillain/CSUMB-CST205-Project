286ffb34b99aac2f06ad525a05ca0040b761f1aa
Marc Villain <marc.villain@epita.fr>
2018-05-14 12:53:39 -0700

    [ADD] Header in start.py

--- start.py
+++ start.py
@@ -1,3 +1,20 @@
+"""
+Pytoshop
+---
+Paint-like/photoshop-like tool, allowing people to draw, add
+filters and do all kinds of modifications to their images.
+
+This project was developed as part of a California State University of
+Monterey Bay project for the CST205 course.
+"""
+
+__author__ = "Pernille Dahl, Joey Thomas, Marc Villain, Sam Westigard"
+__copyright__ = "Copyright 2018, Pytoshop Team"
+__date__ = "05-14-2018"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__status__ = "Production"
+
 from pytoshop import pytoshop
 from pytoshop.pytoshop import Pytoshop
 
